f8aca359674189a62b3062a3152f4d60468dc8be
julianw113 <julianlwilson@gmail.com>
2021-03-14 06:08:23 -0500

    persistent draft 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,92 @@
 import 'package:flutter/material.dart';
 //import 'package:location/location.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(TopLevelWidget());
 
-/// This is the main application widget.
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class TopLevelWidget extends StatelessWidget {
+  final navigatorKey = GlobalKey<NavigatorState>();
 
-  static const String _title = 'Disastrr App';
-
+  final pagesRouteFactories = {
+    "/": () => MaterialPageRoute(
+          builder: (context) => Center(
+            child: Text(
+              "Home",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ),
+        ),
+    "chat": () => MaterialPageRoute(
+          builder: (context) => Center(
+            child: Text(
+              "Chat",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ),
+        ),
+    "trade": () => MaterialPageRoute(
+          builder: (context) => Center(
+            child: Text(
+              "Trade",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ),
+        ),
+    "events": () => MaterialPageRoute(
+          builder: (context) => Center(
+            child: Text(
+              "Events",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ),
+        ),
+    "info": () => MaterialPageRoute(
+          builder: (context) => Center(
+            child: Text(
+              "Info",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ),
+        ),
+    "getHelp": () => MaterialPageRoute(
+          builder: (context) => Center(
+            child: Text(
+              "Get Help",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ),
+        ),
+  };
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
-    );
-  }
-}
-
-/// This is the stateful widget that the main application instantiates.
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Live Chat',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Trade',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Live Events',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Information',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 4: Get Help',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        backgroundColor: Colors.teal[900],
-        title: const Text('Disastrr'),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.teal[900],
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: 'Live Chat',
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.teal[900],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Trade',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.privacy_tip),
-            label: 'Live Events',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info),
-            label: 'Information',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.help),
-            label: 'Get Help',
-          ),
+          body: _buildBody(),
+          bottomNavigationBar: _buildBottomNavigationBar(context),
+        ),
+      );
+
+  Widget _buildBody() => MaterialApp(
+      navigatorKey: navigatorKey,
+      onGenerateRoute: (route) => pagesRouteFactories[route.name]!());
+
+  Widget _buildBottomNavigationBar(context) => BottomNavigationBar(
+        items: [
+          _buildBottomNavigationBarItem("Home", Icons.home),
+          _buildBottomNavigationBarItem("Chat", Icons.chat),
+          _buildBottomNavigationBarItem("Trade", Icons.people),
+          _buildBottomNavigationBarItem("Events", Icons.warning),
+          _buildBottomNavigationBarItem("Info", Icons.info),
+          _buildBottomNavigationBarItem("Get Help", Icons.help)
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.teal[400],
-        onTap: _onItemTapped,
-      ),
-    );
-  }
+        onTap: (routeIndex) => navigatorKey.currentState!
+            .pushNamed(pagesRouteFactories.keys.toList()[routeIndex]),
+      );
+
+  _buildBottomNavigationBarItem(name, icon) => BottomNavigationBarItem(
+      // ignore: deprecated_member_use
+      icon: Icon(icon),
+      title: Text(name),
+      backgroundColor: Colors.black45);
 }
